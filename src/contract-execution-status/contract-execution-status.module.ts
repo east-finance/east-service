787fd899dcb2d96a851256d12e62151b6fc8219d
mtokarev <mtokarev@web3tech.ru>
2021-07-26 19:04:42 +0300

    WE-7189

--- src/contract-execution-status/contract-execution-status.module.ts
+++ src/contract-execution-status/contract-execution-status.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { WeSdkFactory } from '../common/we-sdk.provider';
 import { ConfigModule } from '../config/config.module';
+import { DatabaseModule } from '../database/database.module';
 import { ContracExecutiontStatusService } from './contract-execution-status.service';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, DatabaseModule],
   providers: [ContracExecutiontStatusService, WeSdkFactory],
 })
 export class ContracExecutiontStatusModule {}
