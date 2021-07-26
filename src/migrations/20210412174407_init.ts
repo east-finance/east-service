6415aaafa8282628867895e4edbd08b34bdd273c
mtokarev <mtokarev@web3tech.ru>
2021-07-26 19:06:24 +0300

    WE-7189

--- src/migrations/20210412174407_init.ts
+++ src/migrations/20210412174407_init.ts
@@ -84,6 +84,21 @@ export async function up(knex: Knex): Promise<void> {
         CREATE INDEX IF NOT EXISTS transactions_log_address ON public.transactions_log USING btree (height, created_at);
         CREATE INDEX IF NOT EXISTS transactions_log_order ON public.transactions_log USING btree (address);
         CREATE INDEX IF NOT EXISTS tvault_log_address ON public.vault_log USING btree (address);
+
+        CREATE TYPE contract_execution_status AS ENUM ('pending', 'success', 'fail');
+
+        CREATE TABLE user_transaction_statuses (
+          id               SERIAL                                   PRIMARY KEY,
+          tx_id            character varying                        NOT NULL,
+          address          character varying                        NOT NULL,
+          status           contract_execution_status                NOT NULL,
+          type             tx_type                                  NOT NULL,
+          error            character varying,
+          UNIQUE(tx_id, address, status)
+        );
+  
+        CREATE INDEX user_transaction_statuses_address ON user_transaction_statuses (address);
+        CREATE INDEX user_transaction_statuses_status ON user_transaction_statuses (status);
     `)
 }
 
