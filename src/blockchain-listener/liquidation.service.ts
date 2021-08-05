01aafbc977e5c97ba42a68180c7144340d574bda
mtokarev <mtokarev@web3tech.ru>
2021-08-05 19:34:44 +0300

    refactoring

--- src/blockchain-listener/liquidation.service.ts
+++ src/blockchain-listener/liquidation.service.ts
@@ -1,11 +1,9 @@
-import { Injectable, Inject, Logger } from '@nestjs/common'
+import { Injectable, Inject } from '@nestjs/common'
 import { ConfigService } from '../config/config.service'
 import {
   DB_CON_TOKEN,
   Tables,
-  WE_SDK_PROVIDER_TOKEN
 } from '../common/constants'
-import { WeSdk } from '@wavesenterprise/js-sdk'
 import { Knex } from 'knex'
 import { PersistService } from './persist.service'
 
@@ -16,7 +14,6 @@ export class LiquidationService {
   constructor (
     private readonly configService: ConfigService,
     @Inject(DB_CON_TOKEN) readonly knex: Knex,
-    @Inject(WE_SDK_PROVIDER_TOKEN) private readonly weSdk: WeSdk,
     private readonly persistService: PersistService
   ) {}
 
@@ -36,36 +33,5 @@ export class LiquidationService {
       .where('is_active', true)
       .andWhereRaw(`west_amount / east_amount * ? < ?`, [ coef, LIQUIDATION_COLLATERAL ])
       .leftJoin(Tables.VaultLog, `vaults.id`, `${Tables.VaultLog}.id`)
-  }
-
-  async checkVaults() {
-    const { westRate } = await this.persistService.getLastOracles()
-    const liquidatedVaults = await this.getLiquidatableVaults()
-
-    if (liquidatedVaults.length) {
-      Logger.warn(`liquidatedVaults: ${liquidatedVaults.map(v => JSON.stringify(v)).join('\n')}`)
-      Logger.warn(`westRate: ${JSON.stringify(westRate)}`)
-      await Promise.all(liquidatedVaults.map(v =>
-        this.liquidateVault(v).catch(err => {
-          Logger.error(`Error while liquidating vault: ${JSON.stringify(v)}`)
-          Logger.error(err)
-        })
-      ))
-    }
-  }
-
-  async liquidateVault(vault: any) {
-    const liquidateCall = this.weSdk.API.Transactions.CallContract.V4({
-      contractId: this.configService.envs.EAST_CONTRACT_ID,
-      contractVersion: 1,
-      timestamp: Date.now(),
-      params: [{
-        type: 'string',
-        key: 'liquidate',
-        value: JSON.stringify({ address: vault.address})
-      }]
-    })
-
-    await liquidateCall.broadcastGrpc(this.configService.getKeyPair())
   }
 }
